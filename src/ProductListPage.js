396148dfe3825290e4a9076f411ca9db68dcfda8
SoJaeBeom <sojb0826@gmail.com>
2022-05-24 01:02:21 +0900

    [feat] 상품 목록 페이지 구현

--- src/ProductListPage.js
+++ src/ProductListPage.js
@@ -1,3 +1,6 @@
+import { request } from './api.js'
+import ProductList from './ProductList.js'
+
 export default function ProductListPage({ $target }) {
   const $page = document.createElement('div')
   $page.className = 'ProductListPage'
@@ -6,4 +9,20 @@ export default function ProductListPage({ $target }) {
   this.render = () => {
     $target.appendChild($page)
   }
+
+  this.setState = (nextState) => {
+    this.state = nextState
+  }
+
+  const fetchProducts = async () => {
+    const products = await request('/products')
+    this.setState(products)
+  }
+
+  const productList = new ProductList({
+    $target: $page,
+    initialState: this.state,
+  })
+
+  fetchProducts()
 }
