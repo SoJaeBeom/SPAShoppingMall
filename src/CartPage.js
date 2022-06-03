f4b4a243a089e78196208d74c97840c36d3399d1
SoJaeBeom <sojb0826@gmail.com>
2022-06-03 21:02:32 +0900

    [feat] 장바구니 데이터가 없을 경우 alert처리

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,10 +1,24 @@
+import { routeChange } from './router.js'
+import { getItem } from './storage.js'
+
 export default function CartPage({ $target }) {
   const $page = document.createElement('div')
   $page.className = 'CartPage'
 
   $page.innerHTML = '<h1>장바구니</h1>'
 
+  const cartData = getItem('products_cart', [])
+
+  this.state = {
+    products: null,
+  }
+
   this.render = () => {
-    $target.appendChild($page)
+    if (cartData.length === 0) {
+      alert('장바구니가 비어있습니다.')
+      routeChange('/')
+    } else {
+      $target.appendChild($page)
+    }
   }
 }
